32981433a9cfbb9227275cb565d803d39d565bda
Mariano <neosrash351@gmail.com>
2024-11-05 00:12:10 -0300

    carga de imagenes y inicio de clase hornerito

--- FlappyHornerito.py
+++ FlappyHornerito.py
@@ -1,8 +1,10 @@
 import pygame
 from pygame.locals import *
-#hola
+
+#inicio owo
+
 pygame.init()
-print("hola")
+
 ancho_pantalla = 664
 largo_pantalla = 736
 
@@ -10,12 +12,30 @@ pantalla = pygame.display.set_mode((ancho_pantalla, largo_pantalla)) #creamos la
 pygame.display.set_caption('Flappy Hornerito')
 
 run = True
-#owo
+
 #Imagenes cargadas
 bg = pygame.image.load('bg.png')
 
+#clase del hornerito
+
+
+class Hornerito(pygame.sprite.Sprite)
+    def __init__(self, x, y):
+        pygame.sprite.Sprite. __init__ (self)
+        self.image=pygame.image.load("")
+        self.rect= self.image.get_rect ()
+        self.rect.center =[x, y]
+
+Hornerito_grupo = pygame.sprite.Group()
+flappy= Hornerito (100,int(largo_pantalla/2))
+Hornerito_grupo.add(flappy)
+
+pygame.quit()
+
 while run: #ciclo de ejecucion del juego
     pantalla.blit(bg, (0,0)) #cargamos el fondo
+
+    Hornerito_grupo.draw(pantalla)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -24,9 +44,3 @@ while run: #ciclo de ejecucion del juego
     pygame.display.update() #actualiza todo lo que esta en pantalla
 
 
-#hola
-
-
-
-
-pygame.quit()
