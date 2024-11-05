62630337bae9f4e88ef0709d55f4d158ba5e7de6
Horacio Sutara <rhsuty@gmail.com>
2024-11-05 06:08:58 -0300

    Comentarios de los archivos

--- FlappyHornerito.py
+++ FlappyHornerito.py
@@ -3,23 +3,24 @@ import Constantes as const
 import Personaje as pj
 from pygame.locals import *
 
+#limitador de fps (si se cambia se debe ajustar los parametros de velocidad y gravedad)
 reloj = pygame.time.Clock()
 
 pygame.init()
 
-
+#creacion de pantalla y nombre
 pantalla = pygame.display.set_mode((const.ancho_pantalla, const.largo_pantalla)) #creamos la pantalla
 pygame.display.set_caption('Flappy Hornerito')
 
 run = True
 
-#Imagenes cargadas
+#Imagen de fondo
 bg = pygame.image.load(const.fondo_imagen)
 
-
-Hornerito_grupo = pygame.sprite.Group()
-flappy= pj.Hornerito (const.Posicion_x,const.Posicion_y,pantalla)
-Hornerito_grupo.add(flappy)
+#Crear personaje
+Hornerito_grupo = pygame.sprite.Group()#Maneja los sprte del pj
+flappy= pj.Hornerito (const.Posicion_x,const.Posicion_y,pantalla) #crea al pj
+Hornerito_grupo.add(flappy)# asigna los sprites al pj
 
 
 while run: #ciclo de ejecucion del juego
@@ -27,21 +28,21 @@ while run: #ciclo de ejecucion del juego
     
     pantalla.blit(bg, (0,0)) #cargamos el fondo
 
-    Hornerito_grupo.draw(pantalla)
-    Hornerito_grupo.update()
-    flappy.caer()
+    Hornerito_grupo.draw(pantalla)#dibuja al pj en la pantalla
+    Hornerito_grupo.update()# cambia el sprite
+    flappy.caer()# actua la gravedad en el pj
 
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+        if event.type == pygame.QUIT:# comando para cerrar la pantalla
             run = False
 
-        if event.type==pygame.KEYDOWN:
+        if event.type==pygame.KEYDOWN:# detecta la presion del teclado
             
-            if event.key == pygame.K_w or event.key == pygame.K_SPACE:
-                flappy.mover_arriba()
+            if event.key == pygame.K_w or event.key == pygame.K_SPACE:#detecta w o space
+                flappy.mover_arriba()# activa el salto
 
     pygame.display.update() #actualiza todo lo que esta en pantalla
 
 
-pygame.quit()
+pygame.quit()# Cierra el programa
 
--- Personaje.py
+++ Personaje.py
@@ -2,12 +2,12 @@ import pygame
 import Constantes as const
 class Hornerito(pygame.sprite.Sprite):
     def __init__(self, x, y,interfaz):
-        self.ventana=interfaz
-        self.largo_ventana,self.alto_ventana=interfaz.get_size()
-        self.pos_x=x
-        self.pos_y=y
+        self.ventana=interfaz # asigna ventana a la que pertenece
+        self.largo_ventana,self.alto_ventana=interfaz.get_size() #dimensiones de la ventana
+        self.pos_x=x #posicion de inicio en x
+        self.pos_y=y #posicion de inicio en y
 
-        self.velocidad=0
+        self.velocidad=0 # velocidad inicial del pj (estatico)
 
         pygame.sprite.Sprite. __init__ (self)
         self.indice =0
@@ -28,23 +28,23 @@ class Hornerito(pygame.sprite.Sprite):
                     self.indice= 0
             self.image=const.imagenes_Hornero[self.indice]
     
-    def mover_arriba(self,velocidad=const.Velocidad_personaje):
-        self.velocidad=velocidad
-        self.pos_y-=velocidad
-        if const.Alto_personaje/2>self.pos_y:
+    def mover_arriba(self,velocidad=const.Velocidad_personaje): # funcion del salto del pajaro
+        self.velocidad=velocidad #impulso inicial
+        self.pos_y-=velocidad # traslacion en eje y
+        if const.Alto_personaje/2>self.pos_y: # limita los bordes de la pantalla
             self.pos_y=const.Alto_personaje/2
-        self.rect.center=(self.pos_x,self.pos_y)
+        self.rect.center=(self.pos_x,self.pos_y) #nueva posicion
 
-    def mover_abajo(self,velocidad=const.Velocidad_personaje):
-        self.pos_y+=velocidad
-        if self.alto_ventana<self.pos_y:
+    def mover_abajo(self,velocidad=const.Velocidad_personaje):# funcion de mover hacia abajo
+        self.pos_y+=velocidad #traslacion hacia abajo
+        if self.alto_ventana<self.pos_y:# limita borde inferior
             self.pos_y=self.alto_ventana-1
             
-        elif const.Alto_personaje/2>self.pos_y:
+        elif const.Alto_personaje/2>self.pos_y:# limita borde superior 
             self.pos_y=const.Alto_personaje/2+1
             self.velocidad=0
-        self.rect.center=(self.pos_x,self.pos_y)
+        self.rect.center=(self.pos_x,self.pos_y)# nueva posicion
 
     def caer(self):
-        self.velocidad+=const.gravedad
+        self.velocidad+=const.gravedad # gravedad del personaje
         self.mover_abajo(self.velocidad)
