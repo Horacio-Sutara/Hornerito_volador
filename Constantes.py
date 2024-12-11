bae2707d61150a7df9fb39b1e4be58efcadfc2c0
Horacio Sutara <rhsuty@gmail.com>
2024-12-11 19:49:15 -0300

    Ordenar por carpetas

--- Constantes.py
+++ Constantes.py
@@ -23,48 +23,48 @@ ROJO = (255,0,0)
 
 
 # sonido
-puntaje_sonido="puntaje.wav"
-fondo_sonido="fondo.wav"
-muerte_sonido="muerte.wav"
-boton_sonido="boton.wav"
+puntaje_sonido="Sound//puntaje.wav"
+fondo_sonido="Sound//fondo.wav"
+muerte_sonido="Sound//muerte.wav"
+boton_sonido="Sound//boton.wav"
 
 
 # imagenes (sprites)
-jugar_imagen="boton-jugar.png"
-jugar_imagen_ampliacion="boton-jugar1.png"
+jugar_imagen="image//boton-jugar.png"
+jugar_imagen_ampliacion="image//boton-jugar1.png"
 
-salir_imagen="boton-salir.png"
-salir_imagen_ampliacion="boton-salir1.png"
+salir_imagen="image//boton-salir.png"
+salir_imagen_ampliacion="image//boton-salir1.png"
 
-fondo_imagen='bg.png'
-fondo_perder="fondo-muerte.png"
+fondo_imagen='image//bg.png'
+fondo_perder="image//fondo-muerte.png"
 
-retry_imagen="Volver-a-jugar.png"
-retry_imagen_ampliacion="Volver-a-jugar1.png"
+retry_imagen="image//Volver-a-jugar.png"
+retry_imagen_ampliacion="image//Volver-a-jugar1.png"
 
-ir_menu_imagen="Volver-al-menu.png"
-ir_menu_imagen_ampliacion="Volver-al-menu1.png"
+ir_menu_imagen="image//Volver-al-menu.png"
+ir_menu_imagen_ampliacion="image//Volver-al-menu1.png"
 
-imagen_arbol_ultra_superior=[pygame.image.load("ultra-arriba.png")]
-imagen_arbol_mega_superior=[pygame.image.load("mega-arriba.png")]
-imagen_arbol_super_superior=[pygame.image.load("super-arriba.png")]
-imagen_arbol_alto_superior=[pygame.image.load("alto-arriba.png")]
-imagen_arbol_medio_superior=[pygame.image.load("medio-arriba.png")]
-imagen_arbol_bajo_superior=[pygame.image.load("bajo-arriba.png")]
-imagen_arbol_chato_superior=[pygame.image.load("chato-arriba.png")]
-imagen_arbol_piso_superior=[pygame.image.load("piso-arriba.png")]
+imagen_arbol_ultra_superior=[pygame.image.load("image//ultra-arriba.png")]
+imagen_arbol_mega_superior=[pygame.image.load("image//mega-arriba.png")]
+imagen_arbol_super_superior=[pygame.image.load("image//super-arriba.png")]
+imagen_arbol_alto_superior=[pygame.image.load("image//alto-arriba.png")]
+imagen_arbol_medio_superior=[pygame.image.load("image//medio-arriba.png")]
+imagen_arbol_bajo_superior=[pygame.image.load("image//bajo-arriba.png")]
+imagen_arbol_chato_superior=[pygame.image.load("image//chato-arriba.png")]
+imagen_arbol_piso_superior=[pygame.image.load("image//piso-arriba.png")]
 
-imagen_arbol_ultra_inferior=[pygame.image.load("ultra-abajo.png")]
-imagen_arbol_mega_inferior=[pygame.image.load("mega-abajo.png")]
-imagen_arbol_super_inferior=[pygame.image.load("super-abajo.png")]
-imagen_arbol_alto_inferior=[pygame.image.load("alto-abajo.png")]
-imagen_arbol_medio_inferior=[pygame.image.load("medio-abajo.png")]
-imagen_arbol_bajo_inferior=[pygame.image.load("bajo-abajo.png")]
-imagen_arbol_chato_inferior=[pygame.image.load("chato-abajo.png")]
-imagen_arbol_piso_inferior=[pygame.image.load("piso-abajo.png")]
+imagen_arbol_ultra_inferior=[pygame.image.load("image//ultra-abajo.png")]
+imagen_arbol_mega_inferior=[pygame.image.load("image//mega-abajo.png")]
+imagen_arbol_super_inferior=[pygame.image.load("image//super-abajo.png")]
+imagen_arbol_alto_inferior=[pygame.image.load("image//alto-abajo.png")]
+imagen_arbol_medio_inferior=[pygame.image.load("image//medio-abajo.png")]
+imagen_arbol_bajo_inferior=[pygame.image.load("image//bajo-abajo.png")]
+imagen_arbol_chato_inferior=[pygame.image.load("image//chato-abajo.png")]
+imagen_arbol_piso_inferior=[pygame.image.load("image//piso-abajo.png")]
 
 imagenes_Hornero=[]
-imagenes_Hornero_colision=[pygame.image.load("Hornerito-muerto1.png"),pygame.image.load("Hornerito-muerto2.png"),pygame.image.load("Hornerito-muerto3.png")]
+imagenes_Hornero_colision=[pygame.image.load("image//Hornerito-muerto1.png"),pygame.image.load("image//Hornerito-muerto2.png"),pygame.image.load("image//Hornerito-muerto3.png")]
 for auxiliar in range (1,5):
-            img = pygame.image.load(f"horneritoSprite{auxiliar}.png")
+            img = pygame.image.load(f"image//horneritoSprite{auxiliar}.png")
             imagenes_Hornero.append(img)
